22190c5f6901a0bc2d9d51d1a5ed78501bc95ff5
Jitender Kumar <ohyajk@gmail.com>
2022-11-28 20:29:27 +0530

    Add remove and localstorage

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-const booksArr = []
-const booksIn = document.querySelector('#booksIn')
+const booksArr = [];
+const booksIn = document.querySelector('#booksIn');
 
-const add = document.querySelector('#add')
-
+const add = document.querySelector('#add');
 
 add.addEventListener('click', () => {
-        const title = document.querySelector('#title')
-        const author = document.querySelector('#author')
-        let model = {title:title.value,author:author.value}
-        const getBooks_1 = JSON.parse(localStorage.getItem("books"))
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const model = { title: title.value, author: author.value };
+  const getBooks1 = JSON.parse(localStorage.getItem('books'));
 
-        if(getBooks_1 == undefined) {
-                booksArr.push(model)
-                localStorage.setItem("books", JSON.stringify(booksArr));
-        }else{
-                getBooks_1.push(model)
-                localStorage.setItem("books", JSON.stringify(getBooks_1));
-        }
-        location.reload();
-    
-})
+  if (getBooks1 === undefined) {
+    booksArr.push(model);
+    localStorage.setItem('books', JSON.stringify(booksArr));
+  } else {
+    getBooks1.push(model);
+    localStorage.setItem('books', JSON.stringify(getBooks1));
+  }
+  window.reload();
+});
+
+const getBooks = JSON.parse(localStorage.getItem('books'));
+if (getBooks === undefined) {
+  booksIn.innerHTML += '<div></div>';
+} else {
+  for (let i = 0; i < getBooks.length; i + 1) {
+    const temp = i;
+    booksIn.innerHTML += `<p>Title : ${getBooks[i].title}</p><p>Author : ${getBooks[i].author}</p><button class="btn-rem" data-id="${temp}">Remove</button>`;
+  }
+}
+const btnRems = document.querySelectorAll('.btn-rem');
+btnRems.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    let target = e.currentTarget.dataset;
+    target = +target.id[target.id.length - 1];
+    const getBooks2 = JSON.parse(localStorage.getItem('books'));
+    getBooks2.splice(target, 1);
+    localStorage.setItem('books', JSON.stringify(getBooks2));
+    window.reload();
+  });
+});
